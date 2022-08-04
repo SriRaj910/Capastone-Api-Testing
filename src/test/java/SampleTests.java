29caf24465acdfd992664a1c9a39e2ba82cbea78
Rajat Srivastava <rajat.srivastava1@swiggy.in>
2022-08-05 05:01:14 +0530

    Added should get user testcase

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -1,9 +1,18 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class SampleTests {
     @Test
-    public  void shouldTestSomething(){
-        Assert.assertEquals(1,1);
+    public  void shouldGetAllUsers(){
+        given()
+                .header("app-id","62ec52e349dc977737dae934")
+                .when()
+                    .get("https://dummyapi.io/data/v1/user")
+                .then()
+                    .statusCode(200)
+                    .log().body();
+
     }
 }
