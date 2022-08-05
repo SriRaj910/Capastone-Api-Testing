30e5121661e001633fb6577ad03498bbde1e1afa
Rajat Srivastava <rajat.srivastava1@swiggy.in>
2022-08-05 06:40:35 +0530

    added test case of not creating exixting user

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -7,26 +7,25 @@ import static io.restassured.RestAssured.given;
 
 public class SampleTests {
     @Test
-    public  void shouldGetAllUsers(){
+    public void shouldGetAllUsers() {
         given()
-                .header("app-id","62ec52e349dc977737dae934")
+                .header("app-id", "62ec52e349dc977737dae934")
                 .when()
-                    .get("https://dummyapi.io/data/v1/user")
+                .get("https://dummyapi.io/data/v1/user")
                 .then()
-                    .statusCode(200)
-                    .body("data",Matchers.hasSize(20))
-                    .body("data",Matchers.hasItem(Matchers.hasEntry("title","mrs")))
-                    .log().body();
+                .statusCode(200)
+                .body("data", Matchers.hasSize(20))
+                .body("data", Matchers.hasItem(Matchers.hasEntry("title", "mrs")))
+                .log().body();
 
     }
 
     @Test
-    public void shouldCreateUser()
-    {
+    public void shouldCreateUser() {
         given()
                 .accept(ContentType.JSON)
                 .contentType(ContentType.JSON)
-                .header("app-id","62ec52e349dc977737dae934")
+                .header("app-id", "62ec52e349dc977737dae934")
                 .body("{\n" +
                         "  \"title\": \"mr\",\n" +
                         "  \"firstName\": \"Rohan\",\n" +
@@ -35,26 +34,49 @@ public class SampleTests {
                         "  \"email\": \"suyashsrivastav967@gmail.com\"\n" +
                         "}")
                 .when()
-                    .post("https://dummyapi.io/data/v1/user/create")
+                .post("https://dummyapi.io/data/v1/user/create")
                 .then()
-                    .statusCode(200)
-                    .log().body();
-
-
+                .statusCode(200)
+                .log().body();
 
 
     }
 
     @Test
-    public void getUsersCreatedByMe()
-    {
+    public void getUsersCreatedByMe() {
         given()
-                .header("app-id","62ec52e349dc977737dae934")
+                .header("app-id", "62ec52e349dc977737dae934")
                 .when()
-                    .get("https://dummyapi.io/data/v1/user?created=1")
+                .get("https://dummyapi.io/data/v1/user?created=1")
                 .then()
-                    .statusCode(200)
-                    .body("data",Matchers.hasItem(Matchers.hasEntry("title","mrs")))
-                    .log().body();
+                .statusCode(200)
+                .body("data", Matchers.hasItem(Matchers.hasEntry("title", "mrs")))
+                .log().body();
     }
-}
+
+    @Test
+    public void shouldNotCreateExistingUser(){
+
+            given()
+                    .accept(ContentType.JSON)
+                    .contentType(ContentType.JSON)
+                    .header("app-id","62ec52e349dc977737dae934")
+                    .body("{\n" +
+                            "  \"title\": \"mr\",\n" +
+                            "  \"firstName\": \"Rohan\",\n" +
+                            "  \"lastName\": \"Patel\",\n" +
+                            "  \"picture\": \"https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80\",\n" +
+                            "  \"email\": \"suyashsrivastav967@gmail.com\"\n" +
+                            "}")
+                    .when()
+                    .post("https://dummyapi.io/data/v1/user/create")
+                    .then()
+                    .statusCode(400)
+                    .log().body();
+
+
+
+
+        }
+    }
+
