0984a7a723e554b8d6305640d59422c46b8436dd
Rajat Srivastava <rajat.srivastava1@swiggy.in>
2022-08-05 06:35:06 +0530

    Added the testcase of getting all users created by me

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -44,4 +44,17 @@ public class SampleTests {
 
 
     }
+
+    @Test
+    public void getUsersCreatedByMe()
+    {
+        given()
+                .header("app-id","62ec52e349dc977737dae934")
+                .when()
+                    .get("https://dummyapi.io/data/v1/user?created=1")
+                .then()
+                    .statusCode(200)
+                    .body("data",Matchers.hasItem(Matchers.hasEntry("title","mrs")))
+                    .log().body();
+    }
 }
