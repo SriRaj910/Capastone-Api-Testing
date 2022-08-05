11b3b8814127eb5aaaa4880b7bf09f0dfc5a6785
Rajat Srivastava <rajat.srivastava1@swiggy.in>
2022-08-05 05:50:49 +0530

    Added createuser test case

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -1,3 +1,5 @@
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,6 +15,30 @@ public class SampleTests {
                 .then()
                     .statusCode(200)
                     .log().body();
+
+    }
+
+    @Test
+    public void shouldCreateUser()
+    {
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("app-id","62ec52e349dc977737dae934")
+                .body("{\n" +
+                        "  \"title\": \"mrs\",\n" +
+                        "  \"firstName\": \"Riya\",\n" +
+                        "  \"lastName\": \"Gupta\",\n" +
+                        "  \"picture\": \"https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80\",\n" +
+                        "  \"email\": \"suyashsrivastav962@gmail.com\"\n" +
+                        "}")
+                .when()
+                    .post("https://dummyapi.io/data/v1/user/create")
+                .then()
+                    .statusCode(200)
+                    .log().body();
+
+
 
     }
 }
