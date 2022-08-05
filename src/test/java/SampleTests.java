2c374f472b23b2f5450c291e6c2b76d9a74c8fda
Rajat Srivastava <rajat.srivastava1@swiggy.in>
2022-08-05 06:27:11 +0530

    Assertion in get user test

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -14,6 +14,8 @@ public class SampleTests {
                     .get("https://dummyapi.io/data/v1/user")
                 .then()
                     .statusCode(200)
+                    .body("data",Matchers.hasSize(20))
+                    .body("data",Matchers.hasItem(Matchers.hasEntry("title","mrs")))
                     .log().body();
 
     }
@@ -26,17 +28,18 @@ public class SampleTests {
                 .contentType(ContentType.JSON)
                 .header("app-id","62ec52e349dc977737dae934")
                 .body("{\n" +
-                        "  \"title\": \"mrs\",\n" +
-                        "  \"firstName\": \"Riya\",\n" +
-                        "  \"lastName\": \"Gupta\",\n" +
+                        "  \"title\": \"mr\",\n" +
+                        "  \"firstName\": \"Rohan\",\n" +
+                        "  \"lastName\": \"Patel\",\n" +
                         "  \"picture\": \"https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80\",\n" +
-                        "  \"email\": \"suyashsrivastav962@gmail.com\"\n" +
+                        "  \"email\": \"suyashsrivastav967@gmail.com\"\n" +
                         "}")
                 .when()
                     .post("https://dummyapi.io/data/v1/user/create")
                 .then()
                     .statusCode(200)
                     .log().body();
+
 
 
 
